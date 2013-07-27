f8455a998901058b6c684f6ff1e9fcff3212cf1c
Quinlan Pfiffer <shithouse@goatse.cx>
2013-07-27 12:49:06 -0700

    Now returns JSON on POST!

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Data.Aeson as A
 import           Data.ByteString.Char8 as BS
 import           Database.KyotoCabinet.Db
 import           Control.Applicative
+import           Control.Monad.IO.Class (liftIO)
 import           Snap.Core
 --import           Snap.Util.FileServe
 import           Snap.Http.Server
 import           System.Environment
+import           Data.Text
+
+-- Datums
+data InsertResult = InsertResult
+    { what_happened :: Text
+    }
+
+instance ToJSON InsertResult where
+    toJSON (InsertResult result) = object ["What happened?" .= result]
 
 
 -- Snap stuff
@@ -22,8 +33,10 @@ site db_location =
     writeBS ("Nothing to see here. " `BS.append` db_location)
 
 submitHandler :: BS.ByteString -> Snap ()
-submitHandler db_location =
-    writeBS "Woo!"
+submitHandler db_location = do
+    result <- liftIO $ kcwithdbopen (BS.unpack db_location) [] [KCOWRITER, KCOCREATE] (insertUrl "ASDF")
+    modifyResponse $ setContentType "application/json"
+    writeLBS $ A.encode $ result
 
 -- General Utils
 runCommand :: [BS.ByteString] -> IO ()
@@ -33,4 +46,13 @@ runCommand _ = BS.putStrLn "Must specify a database location."
 
 -- DB Utils
 urlInDb :: BS.ByteString -> KcDb -> Bool
-urlInDb _ _ = False
+urlInDb url database = True
+
+insertUrl :: BS.ByteString -> KcDb -> IO InsertResult
+insertUrl url db = do
+    case urlInDb url db of
+        True -> return $ InsertResult "Someone tried to submit a duplicate URL."
+        False -> do
+            --kcwithdbcursor db $ \cur -> do
+            --    kccurjump
+            return $ InsertResult "MUDADA"
