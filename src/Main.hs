fd1deae1bc503706e451083ebb814925d4e73c2e
Quinlan Pfiffer <shithouse@goatse.cx>
2013-07-27 15:47:54 -0700

    Working on actually inserting URLs and parsing JSON POST parameters.
    This commit does not compile.

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,9 @@ import           Control.Exception
 import           Data.Aeson as A
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy as LBS
+import           Data.Maybe
+import           Data.Text as T
+import qualified Data.Text.Encoding as TE
 import           Database.KyotoCabinet.Db
 import           Control.Applicative
 import           Control.Monad
@@ -13,9 +16,8 @@ import           Control.Monad.IO.Class (liftIO)
 import           Snap.Core
 --import           Snap.Util.FileServe
 import           Snap.Http.Server
+import           Snap.Extras.JSON
 import           System.Environment
-import           Data.Text as T
-import qualified Data.Text.Encoding as TE
 
 import Prelude hiding (catch)
 
@@ -76,7 +78,11 @@ site db_location =
 
 submitHandler :: BS.ByteString -> Snap ()
 submitHandler db_location = do
-    result <- liftIO $ kcwithdbopen (BS.unpack db_location) [] [KCOWRITER, KCOCREATE] (insertUrl "ASDF")
+    json_param <- getJSON
+    poster <- getParam "person"
+    posted_url <- getParam "url"
+    -- TODO: Don't use fromJust here.
+    result <- liftIO $ kcwithdbopen (BS.unpack db_location) [] [KCOWRITER, KCOCREATE] (insertUrl (fromJust posted_url) (fromJust poster))
     modifyResponse $ setContentType "application/json"
     writeLBS $ A.encode $ result
 
@@ -99,15 +105,21 @@ urlInDb test_url database = do
                     if u == test_url
                     then return $ Right True
                     else do
+                        -- #DEBUG
+                        -- BS.putStrLn $ BS.concat [TE.encodeUtf8 u, ", ", TE.encodeUtf8 test_url]
                         kccurstepback cur
                         loop
                         return $ Right False
-        loop `catch` \(exn::KcException) -> return $ Left (show exn)
+        loop `catch` \(exn::KcException) ->
+            kcdbecode database >>= \error_code ->
+            case error_code of
+                KCENOREC -> return $ Right False
+                err -> return $ Left (show err) -- Something bad happened.
 
-insertUrl :: BS.ByteString -> KcDb -> IO InsertResult
-insertUrl posted_url db = do
+insertUrl :: BS.ByteString -> BS.ByteString -> KcDb -> IO InsertResult
+insertUrl posted_url poster db = do
     in_db_already <- urlInDb (TE.decodeUtf8 posted_url) db
     case in_db_already of
         (Right True) -> return $ InsertResult "Someone tried to submit a duplicate URL."
-        (Right False) -> return $ InsertResult "MUDADA"
+        (Right False) -> return $ InsertResult "TODO"
         (Left msg) -> return $ InsertResult $ T.pack msg
