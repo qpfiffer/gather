7e81d66215b56bf25ffe91d1e16ccbc2b6a35b7f
Quinlan Pfiffer <shithouse@goatse.cx>
2013-07-27 12:13:51 -0700

    Now accepts a commandline argument for the database location.

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import           Data.ByteString.Char8 as BS
+import           Database.KyotoCabinet.Db
 import           Control.Applicative
 import           Snap.Core
-import           Snap.Util.FileServe
+--import           Snap.Util.FileServe
 import           Snap.Http.Server
+import           System.Environment
 
+
+-- Snap stuff
 main :: IO ()
-main = quickHttpServe site
+main = do
+    args <- getArgs
+    runCommand $ fmap BS.pack args
 
-site :: Snap ()
-site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
+site :: BS.ByteString -> Snap ()
+site db_location =
+    ifTop (method POST $ submitHandler db_location) <|>
+    writeBS ("Nothing to see here. " `BS.append` db_location)
 
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+submitHandler :: BS.ByteString -> Snap ()
+submitHandler db_location =
+    writeBS "Woo!"
+
+-- General Utils
+runCommand :: [BS.ByteString] -> IO ()
+runCommand ["-db", db_location] =
+    httpServe defaultConfig $ site db_location
+runCommand _ = BS.putStrLn "Must specify a database location."
+
+-- DB Utils
+urlInDb :: BS.ByteString -> KcDb -> Bool
+urlInDb _ _ = False
