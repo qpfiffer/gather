8a21b52267c3b9dffe3c5ea22cd6c24354edc444
Quinlan Pfiffer <qpfiffer@gmail.com>
2013-07-31 22:09:03 -0700

    Summarization function, take 1.

--- src/Gather/Summarize.hs
+++ src/Gather/Summarize.hs
@@ -2,7 +2,16 @@
 module Gather.Summarize where
 
 import           Data.Text as T
-import           Text.HTML.TagSoup
+import           Text.HTML.TagSoup as TS
 
-summarize :: Integer -> [Tag String] -> T.Text
-summarize summary_size tags = "vavoso"
+combineTags :: String -> String -> String
+combineTags accum val = accum ++ " " ++ val
+
+-- Number of characters to return, parsed tags
+summarize :: Int -> [Tag String] -> T.Text
+summarize summary_size tags =
+    case Prelude.length tags of
+        0 -> "..."
+        _ -> T.pack $ Prelude.take summary_size $
+            Prelude.foldl combineTags " " $
+            Prelude.map fromTagText $ Prelude.filter TS.isTagText tags
